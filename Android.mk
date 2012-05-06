9b9a9cd24ae0e02f1a3b014739d40d22420ad06d
Koushik Dutta <koushd@gmail.com>
2012-05-05 19:52:17 -0700

    Rewrite send_intent to use am start. If you look at ChainsDD's fork of su, you'll see it is littered with backwards/forwards/device compatibility issues. By using the "am" command line tool, which is guaranteed to work because it is used by Eclipse, etc, we will never have parcel compatibility issues again.
    Force su to be statically linked. Can not rely on dynamic linking, due to prelink map changes, missing libs, etc.
    
    Sanitize LD_LIBRARY_PATH and related variables properly.

--- Android.mk
+++ Android.mk
@@ -2,17 +2,17 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := su
-LOCAL_SRC_FILES := utils.c su.c db.c activity.cpp
+LOCAL_SRC_FILES := utils.c su.c db.c activity.c
 
 LOCAL_C_INCLUDES += external/sqlite/dist
 
-LOCAL_SHARED_LIBRARIES := \
+LOCAL_STATIC_LIBRARIES := \
     liblog \
     libsqlite \
-    libbinder \
-    libutils \
+    libc \
 
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := eng
+LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 include $(BUILD_EXECUTABLE)
